1c8f307c946f4ce2b6ca6ff847ff9916d233995a
lasim6206-netizen <lasim6206@gmail.com>
2026-06-09 23:43:38 +0530

    Update encoder4by2tb.v

--- day2/encoder4_2/tb/encoder4by2tb.v
+++ day2/encoder4_2/tb/encoder4by2tb.v
@@ -1,1 +1,25 @@
 
+module encoder4by2_tb(
+
+    );
+    reg [3:0]D;
+    wire [1:0]B;
+    encoder4by2 dut(D,B);
+    initial
+    begin
+    D=0;
+    end
+    initial
+    begin
+    #1
+    D=4'b0001;
+    #1
+    D=4'b0010;
+    #1
+    D=4'b0100;
+    #1
+    D=4'b1000;
+    #1;
+    $finish;
+    end
+endmodule

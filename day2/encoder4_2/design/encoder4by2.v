092b2f59c63a8334194e5147facf4b13497fbfc8
lasim6206-netizen <lasim6206@gmail.com>
2026-06-09 23:42:59 +0530

    Update encoder4by2.v

--- day2/encoder4_2/design/encoder4by2.v
+++ day2/encoder4_2/design/encoder4by2.v
@@ -1,1 +1,15 @@
-
+module encoder4by2(input[3:0]d,output reg [1:0]b);
+always @(*)
+begin
+if (d==4'b0001)
+b=2'b00;
+else if(d==4'b0010)
+b=2'b01;
+else if(d==4'b0100)
+b=2'b10;
+else if(d==4'b1000)
+b=2'b11;
+else
+b=2'bXX;
+end
+endmodule

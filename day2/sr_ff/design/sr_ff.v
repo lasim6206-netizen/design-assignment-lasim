5f5d8f446a94e0e038d75e351595bb3f8f261205
lasim6206-netizen <lasim6206@gmail.com>
2026-06-09 23:47:59 +0530

    Update sr_ff.v

--- day2/sr_ff/design/sr_ff.v
+++ day2/sr_ff/design/sr_ff.v
@@ -1,1 +1,35 @@
 
+module sr_ff(
+    input S,
+    input R,
+    input clk,
+    output reg Q,
+    output reg Qbar
+);
+
+always @(posedge clk)
+begin
+    case ({S,R})
+        2'b00: begin
+            Q    <= Q;      
+            Qbar <= Qbar;
+        end
+
+        2'b01: begin
+            Q    <= 1'b0;   
+            Qbar <= 1'b1;
+        end
+
+        2'b10: begin
+            Q    <= 1'b1; 
+            Qbar <= 1'b0;
+        end
+
+        2'b11: begin
+            Q    <= 1'bx;  
+            Qbar <= 1'bx;
+        end
+    endcase
+end
+
+endmodule

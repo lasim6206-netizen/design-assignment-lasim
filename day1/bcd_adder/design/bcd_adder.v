5d6f839de14a975725af80b416c6b3d18e9a44c7
lasim6206-netizen <lasim6206@gmail.com>
2026-06-09 23:37:49 +0530

    Update bcd_adder.v

--- day1/bcd_adder/design/bcd_adder.v
+++ day1/bcd_adder/design/bcd_adder.v
@@ -1,1 +1,21 @@
+module bcd_adder(
+    input  [3:0] A,
+    input  [3:0] B,
+    input        Cin,
+    output [3:0] Sum,
+    output       Cout
+);
 
+wire [4:0] temp_sum;
+wire [4:0] corrected_sum;
+
+assign temp_sum = A + B + Cin;
+
+assign corrected_sum = (temp_sum > 9) ?
+                       (temp_sum + 5'b00110) :
+                       temp_sum;
+
+assign Sum  = corrected_sum[3:0];
+assign Cout = corrected_sum[4];
+
+endmodule

9d396e8a9644b64c1184b232b93cce0ac0af957c
lasim6206-netizen <lasim6206@gmail.com>
2026-06-11 08:18:47 +0530

    Update in.v

--- day3/face_detector/design/in.v
+++ day3/face_detector/design/in.v
@@ -1,1 +1,16 @@
+module in_module(
+    input clk,
+    input rst,
+    input [7:0] din,
+    output reg [7:0] dout
+);
 
+always @(posedge clk)
+begin
+    if(rst)
+        dout <= 8'd0;
+    else
+        dout <= din;
+end
+
+endmodule

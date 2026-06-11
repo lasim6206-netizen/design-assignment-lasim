a5641149e34853af29ef21575ab79f26705a2e78
lasim6206-netizen <lasim6206@gmail.com>
2026-06-11 08:24:22 +0530

    Update tb.v

--- day3/seq_detect/tb/tb.v
+++ day3/seq_detect/tb/tb.v
@@ -1,1 +1,27 @@
+module sequence_1110_tb(
 
+    );
+    reg clk_tb,rst_tb,din_tb;
+    wire detetcted_tb;
+    sequence_1110 dut(clk_tb,rst_tb,din_tb,detetcted_tb);
+    initial begin
+    {clk_tb,rst_tb,din_tb}=0;
+    end
+    always #5 clk_tb=~clk_tb;
+    initial begin
+    rst_tb=1'b1;
+    #10
+    rst_tb=1'b0;
+    #10
+    din_tb=0;
+    #10
+    din_tb=1;
+    #10
+    din_tb=1;
+    #10
+    din_tb=1;
+    #10;
+    din_tb=0;
+    #10;
+    end
+endmodule

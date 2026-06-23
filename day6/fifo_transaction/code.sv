d6f5b3d596e11c0fa8e863f790bde8c4d9e00c70
lasim6206-netizen <lasim6206@gmail.com>
2026-06-23 10:19:04 +0530

    Update and rename code.v to code.sv

---
+++ day6/fifo_transaction/code.sv
@@ -0,0 +1,25 @@
+class transaction;
+rand bit rst_tb,wrenb_tb,rdenb_tb;
+rand bit [7:0] data_in_tb;
+bit [7:0] data_out_tb;
+bit full,empty;
+constraint c1 {
+    rst_tb   dist {0:=8,1:=2};
+    wrenb_tb dist {0:=2,1:=8};
+    rdenb_tb dist {0:=8,1:=2};
+    data_in_tb dist {
+        8'hFF := 10,
+        8'hAA := 5,
+        8'h55 := 5};
+}
+function void display();
+    $display("rst=%0b wr=%0b rd=%0b din=%0h dout=%0h full=%0b empty=%0b",
+             rst_tb,wrenb_tb,rdenb_tb,
+             data_in_tb,data_out_tb,
+             full,empty);
+endfunction
+
+ndclass
+
+ 
+ 
